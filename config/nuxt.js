65960153ed28345e376586470599eb3a60ed2e49
Islam Magdy <islam@in1t.com>
2019-01-19 15:46:40 +0200

    Updated http2 config

--- config/nuxt.js
+++ config/nuxt.js
@@ -82,6 +82,7 @@ globalName: pkg.name,
 
   render: {
     http2: {
+      push: true,
       pushAssets: (req, res, publicPath, preloadFiles) => preloadFiles
         .filter(f => f.asType === 'script' && f.file === 'runtime.js')
         .map(f => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`)
