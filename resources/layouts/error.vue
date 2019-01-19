84a11ab0c4bfd7e915cadf16c2c019cdf0fec96b
Islam Magdy <islam@in1t.com>
2019-01-19 15:47:26 +0200

    added a new view for error pages

---
+++ resources/layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="error-page">
+    <h1 v-if="error.statusCode === 404">({{ error.statusCode }}) Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Go to Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  layout: 'default'
+}
+</script>
+
+<style lang="scss" scoped>
+.error-page {
+    background: #fff;
+    padding: 20px;
+    min-height: 60vh;
+}
+</style>
