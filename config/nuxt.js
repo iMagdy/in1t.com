49f3c1c36de4408baab600388ed2e5d1f24bac45
Islam Magdy <islam@in1t.com>
2019-01-19 12:47:04 +0200

    added google verification for search console

--- config/nuxt.js
+++ config/nuxt.js
@@ -17,7 +17,8 @@ globalName: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'google-site-verification', content: '2EZPuToreFEYFHRd1onOSsFnRK9cfhClVUJLJtAUi8s' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
